23968d25069b2363d7deb07dc1b6118f65daefc3
benjto <benjaminto2001@gmail.com>
2021-12-06 16:14:20 +0100

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -1,9 +1,9 @@
 <?php
 //declaration des variables
-$database = "Paris Shopping";
+$database = "paris shopping";
 //connectez-vous dans votre BDD
 //Rappel : votre serveur = localhost | votre login = root | votre mot de pass = '' (rien)
-$db_handle = mysqli_connect('localhost', 'root', '' );
+$db_handle = mysqli_connect('localhost', 'root', 'root' );
 $db_found = mysqli_select_db($db_handle, $database);
 $name = isset($_POST["nom"])? $_POST["nom"] : "";
 $email = isset($_POST["email"])? $_POST["email"] : "";
