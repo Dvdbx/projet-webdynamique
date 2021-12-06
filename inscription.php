25c7ebbacedc13996a07a2b06aa12c506db4b9b1
Yohann-cell <58658875+Yohann-cell@users.noreply.github.com>
2021-12-06 12:18:58 +0100

    Modif mot de passe
    L'inscription fonctionne

--- inscription.php
+++ inscription.php
@@ -3,7 +3,7 @@
 $database = "Paris Shopping";
 //connectez-vous dans votre BDD
 //Rappel : votre serveur = localhost | votre login = root | votre mot de pass = '' (rien)
-$db_handle = mysqli_connect('localhost', 'root', 'root' );
+$db_handle = mysqli_connect('localhost', 'root', '' );
 $db_found = mysqli_select_db($db_handle, $database);
 $name = isset($_POST["nom"])? $_POST["nom"] : "";
 $email = isset($_POST["email"])? $_POST["email"] : "";
